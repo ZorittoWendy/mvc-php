707d3de713975ef176e8573c022e260da186439a
ZorittoWendy <zorittoprogramador@gmail.com>
2025-11-20 15:04:43 -0300

    feat: add page content header footer

--- mvc-php/app/Controller/Pages/Page.php
+++ mvc-php/app/Controller/Pages/Page.php
@@ -5,6 +5,22 @@ namespace App\Controller\Pages;
 use App\Utils\View;
 
 class Page{
+    /** 
+     * Método responsável por renderizar o topo da pagina
+     * @return string
+     */
+    private static function getHeader() {
+        return View::render('pages/header');
+    }
+
+     /** 
+     * Método responsável por renderizar o Rodapé da pagina
+     * @return string
+     */
+    private static function getFooter() {
+        return View::render('pages/footer');
+    }
+
     /**
      * Método resposável por retornar o conteúdo (view) da nossa home 
      * @return string
@@ -12,7 +28,9 @@ class Page{
     public static function getPage($title, $content) {
         return View::render('pages/page',[
           'title' => $title,
-          'content' => $content
+          'header' => self::getHeader(),
+          'content' => $content,
+          'footer' => self::getFooter()
         ]);
     }
 }
