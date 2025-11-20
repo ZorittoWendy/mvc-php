6b96d317e5a4a59eccc0466825dc5e3525f75a47
ZorittoWendy <zorittoprogramador@gmail.com>
2025-11-20 15:22:01 -0300

    chore: creating the model entity

---
+++ mvc-php/app/Model/Entity/Organization.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Entity;
+
+class Organization{
+    /**
+     * ID da organização
+     * @var integer
+     */
+    public $id = 1;
+
+    /**
+     * Nome da Organização
+     * @var string
+     */
+    public $name = 'Canal ZORITTO';
+
+    /**
+     * Site da organização
+     * @var string
+     */
+    public $site = 'https://youtube.com';
+
+    /**
+     * descrição da Organização
+     * @var string 
+    */
+    public $description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nemo expedita, qui odio quaerat alias quo! Quasi rem, quisquam numquam obcaecati rerum praesentium atque assumenda eum architecto, perspiciatis, tempora deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quasi deleniti quas dolores impedit modi voluptas molestiae, tempora facere explicabo qui nobis veniam nostrum cumque, sapiente officiis, illum voluptatum sit!';
+}
