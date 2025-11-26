8b325032b0de0d11c8fd8ee246bcc5174e255815
ZorittoWendy <zorittoprogramador@gmail.com>
2025-11-26 20:04:48 -0300

    feat: adding the exceptions

--- mvc-php/index.php
+++ mvc-php/index.php
@@ -1,8 +1,21 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-
+use App\Http\Router;
+use App\Http\Response;
 use App\Controller\Pages\Home;
 
-echo Home::getHome();
+define('URL', 'http://localhost/mvc-php');
+
+$obRouter = new Router(URL);
+
+//ROTA HOME
+$obRouter->get('/', [
+    function () {
+        return new Response(200, Home::getHome());
+    }
+]);
+
+//IMPRIME O RESPONSE DA ROTA
+$obRouter->run()->sendResponse();
