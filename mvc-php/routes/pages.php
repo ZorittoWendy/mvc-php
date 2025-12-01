96ecd5d4668fac2599ab91da82875ee63b32c593
ZorittoWendy <zorittoprogramador@gmail.com>
2025-12-01 16:09:00 -0300

    chore: add routes

---
+++ mvc-php/routes/pages.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Response;
+use App\Controller\Pages;
+
+//ROTA HOME
+$obRouter->get('/', [
+    function () {
+        return new Response(200, Pages\Home::getHome());
+    }
+]);
+
+//ROTA HOME
+$obRouter->get('/sobre', [
+    function () {
+        return new Response(200, Pages\Home::getHome());
+    }
+]);
