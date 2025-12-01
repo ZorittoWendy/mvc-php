db2ef5dd5eea858f3c050b0ed5c238182987b935
ZorittoWendy <zorittoprogramador@gmail.com>
2025-12-01 16:10:06 -0300

    feat: add page home, footer, home, page HTML

--- mvc-php/index.php
+++ mvc-php/index.php
@@ -3,19 +3,14 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Http\Router;
-use App\Http\Response;
-use App\Controller\Pages\Home;
+
 
 define('URL', 'http://localhost/mvc-php');
 
 $obRouter = new Router(URL);
 
-//ROTA HOME
-$obRouter->get('/', [
-    function () {
-        return new Response(200, Home::getHome());
-    }
-]);
+//INCLUI AS ROTAS DE PAGINAS
+include __DIR__ . '/routes/pages.php';
 
 //IMPRIME O RESPONSE DA ROTA
 $obRouter->run()->sendResponse();
