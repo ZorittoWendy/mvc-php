2107f4850a88e01f3fd443e47ee9e7139b022aef
ZorittoWendy <zorittoprogramador@gmail.com>
2025-12-02 15:09:30 -0300

    feat: add the variables from the view

--- mvc-php/app/Utils/View.php
+++ mvc-php/app/Utils/View.php
@@ -2,15 +2,32 @@
 
 namespace App\Utils;
 
-class View{
+class View
+{
+    /**
+     * Variaveis padroes da View
+     * @var array
+     */
+    private static $vars = [];
+
+    /**
+     * Metodo responsavel por definir os dados iniciais da classe
+     * @param array $vars
+     */
+    public static function init($vars = [])
+    {
+        self::$vars = $vars;
+    }
+
     /** 
      * Metódo responsável por retornar o conteúdo de uma view
      * @param string
      * @return string
      */
-    private static function getContentView($view) {
-          $file = __DIR__.'/../../resources/view/'.$view.'.html';
-          return file_exists($file) ? file_get_contents($file) : '';
+    private static function getContentView($view)
+    {
+        $file = __DIR__ . '/../../resources/view/' . $view . '.html';
+        return file_exists($file) ? file_get_contents($file) : '';
     }
 
     /** 
@@ -19,18 +36,21 @@ class View{
      * @param array $vars (string/numeric)
      * @param string
      */
-    public static function render($view, $vars = []) {
+    public static function render($view, $vars = [])
+    {
         //CONTEÚDO DA VIEW
         $contentView = self::getContentView($view);
+
+        //MERGE DE VARIAVEIS DA VIEW
+        $vars = array_merge(self::$vars, $vars);
 
         //CHAVES DO ARRAY DE VARIÁVEIS 
         $keys = array_keys($vars);
         $keys = array_map(function ($item) {
-            return '{{'.$item.'}}';
+            return '{{' . $item . '}}';
         }, $keys);
 
         //RETORNA O CONTEUDO RENDERIZADO
         return str_replace($keys, array_values($vars), $contentView);
-        
     }
 }
